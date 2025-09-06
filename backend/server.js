7d67666762d9cf14ab81102ec6375cb69ea70ba7
Dhairya Luthra <dhairya8luthra@gmail.com>
2025-09-06 06:42:12 +0530

    added cors .env middleware to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
-
+import pingRoute from './routes/ping.js';
 const app = express();
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || '*';
+app.use(cors({ origin: CORS_ORIGIN }));
+app.use(express.json()); // for JSON request bodies
 
-app.get('/ping', (req, res) => {
-  res.status(200).send('pong');
-});
+app.use('/api', pingRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
